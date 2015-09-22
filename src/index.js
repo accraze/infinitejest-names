d2846c9602402853050b5960c1e9f0a48b5be44b
accraze <accraze@gmail.com>
2015-09-21 22:03:56 -0700

    feat(random): Add ability to get an array of infinitejest-names
    If you pass a number to the random function, you will receive an array with that number of random
    items
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var infiniteJestNames = require('./infinitejest-names.json');
 var uniqueRandomArray = require('unique-random-array');
+var getRandomItem = uniqueRandomArray(infiniteJestNames);
 
 module.exports = {
   all: infiniteJestNames,
-  random: uniqueRandomArray(infiniteJestNames)
+  random: random
 };
+
+function random(num) {
+  if (num === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i = 0; i < num; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,34 @@
 var expect = require('chai').expect;
 var infiniteJest = require('./index');
 
-describe('infiniteJest', function(){
-  it('should work!', function(){
-    expect(true).to.be.true;
-  })
+describe('infinitejest-names', function(){
+  describe('all', function () {
+    it('should be an arrya of strings', function () {
+      expect(infiniteJest.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string' ;
+        });
+      }
+    });
+
+    it('should contain `Hal Incandenza`', function () {
+      expect(infiniteJest.all).to.include('Hal Incandenza');
+    });
+  });
+  describe('random', function () {
+    it('should return a random item from infiniteJest.all', function () {
+      var randomItem = infiniteJest.random();
+      expect(infiniteJest.all).to.include(randomItem);
+    });
+    it('should return an array of random items if passed a number', function () {
+      var randomItems = infiniteJest.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function(item) {
+        expect(infiniteJest.all).to.include(item);
+      })
+    });
+  });
 })
+
